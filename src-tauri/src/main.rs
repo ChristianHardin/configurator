ca19dadc13bfe46c58af4e5beb7ce23bfeefb4a1
ChristianHardin <christianhardin@proton.me>
2025-01-14 01:39:59 -0500

    feat: added sqlite plugin and table create migrations

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,52 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri_plugin_sql::{Builder, Migration, MigrationKind};
 
 fn main() {
-    anco_revised_lib::run()
+    let migrations = vec![
+        Migration{
+            version: 1,
+            description: "create_category_table",
+            sql: "CREATE TABLE categories (
+                id TEXT PRIMARY KEY,
+                category TEXT UNIQUE NOT NULL,
+                priority INTEGER UNIQUE NOT NULL
+            )",
+            kind: MigrationKind::Up,
+        },
+        Migration{
+            version: 2,
+            description: "create_subcategory_table",
+            sql: "CREATE TABLE subcategories (
+                id TEXT PRIMARY KEY,
+                categoryid TEXT NOT NULL,
+                subcategory TEXT UNIQUE NOT NULL,
+                priority INTEGER UNIQUE NOT NULL,
+                FOREIGN KEY (categoryid) REFERENCES categories(id)
+            )",
+            kind: MigrationKind::Up,
+        },
+        Migration{
+            version: 3,
+            description: "create_items_table",
+            sql: "CREATE TABLE items (
+                id TEXT PRIMARY KEY,
+                subcategoryid TEXT UNIQUE NOT NULL,
+                priority INTEGER UNIQUE NOT NULL,
+                number TEXT,
+                description TEXT,
+                price REAL,
+                FOREIGN KEY (subcategoryid) REFERENCES subcategories(id)
+            )",
+            kind: MigrationKind::Up,
+        },
+    ];
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::default()
+            .add_migrations("sqlite:main.db", migrations)
+            .build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
 }
